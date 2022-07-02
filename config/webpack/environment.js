9d608d0ddd1bc97b9655bc656c928ff912a91b5c
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-02 19:38:40 -0300

    feat: criação app

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
-
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
+}))
+// End new addition
 module.exports = environment
